4b6150c38664ab111bda06f51ae1f6e7c9f61c54
keithlee96 <keithlee96@gmail.com>
2017-07-30 12:35:11 +1000

    added image

--- app/src/main/java/com/equilibrium/yadayada/SubmitRecording.java
+++ app/src/main/java/com/equilibrium/yadayada/SubmitRecording.java
@@ -14,7 +14,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class RecordActivity extends AppCompatActivity {
+public class SubmitRecording extends AppCompatActivity {
     private final int REQ_CODE_SPEECH_INPUT = 100;
 
     private TextView txtSpeechInput;
@@ -46,10 +46,10 @@ public class RecordActivity extends AppCompatActivity {
     private void promptSpeechInput() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
-                        getString(R.string.speech_prompt));
+                getString(R.string.speech_prompt));
         try {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
         } catch (ActivityNotFoundException a) {
